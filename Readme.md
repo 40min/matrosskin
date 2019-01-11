c18bda924f841bda7229c56e4d5992e0ba618810
asorokin <a.sorokin@ueni.com>
2019-01-11 11:52:16 +0300

    * added logging messages into news

--- Readme.md
+++ Readme.md
@@ -1,25 +1,28 @@
 # Todo
     
 * News
-    * Add education of new model based on chat
     * Generate news 
         - from time-to time 
         - mixed with chat-model
         - triggered by words
-    * Check ability to subscribe on news
     * Rate generated news
-    * Save subscribers into db
+    * Add education of new model based on chat
+    * Save subscribers into db (sqlite)
     * Add gen interval into subscription
     
 * Mushrooms
     * Receive/save picture
+    * install mushrooms lib
     
 * Weather
     * Better approve button
+    * Fix weather lib by providing api creds for Yahoo Weather
 
 * Common-tech:
+    * better message routing
     * handle errors
-    * dockerize it
     * create help-page with commands-list
     * add suspend/resume commands
+    * raise broadcast messages for subscribers
+    
     
--- matrosskin/paws/news.py
+++ matrosskin/paws/news.py
@@ -31,32 +31,36 @@ subscribed_chats = set()
 
 def grab_news_callback(bot, job):
     now = datetime.now()
-    logger.info('Grabbing news {}' . format(now.strftime('%Y-%m-%d %H:%M:%S')))
+    logger.info('News: Grabbing news {}' . format(now.strftime('%Y-%m-%d %H:%M:%S')))
     news_added = head_grab.run()
     if news_added:
         fun_generator.reload_model_from_txt()
 
 
 def subscribed_generate_callback(bot, job):
+    logger.info('News: send news to subscribers')
     for chat_id in subscribed_chats:
         fun_txt = fun_generator.make_phrases()
         if fun_txt:
-            bot.send_message(chat_id=chat_id, text=fun_txt[0].encode('utf-8'))
+            bot.send_message(chat_id=chat_id, text=fun_txt[0])
 
 
 @run_async
 def show_news(bot, update):
+    logger.info(f'News: single news request for user {update.message.from_user.username}')
     phrases = fun_generator.make_phrases()
     news_phrase = phrases[0] if phrases else DEFAULT_NEWS
     bot.send_message(chat_id=update.message.chat_id, text=news_phrase)
 
 
 def subscribe(bot, update):
+    logger.info(f'News: subscribed user {update.message.from_user.username}')
     chat_id = update.message.chat_id
     subscribed_chats.add(chat_id)
 
 
 def unsubscribe(bot, update):
+    logger.info(f'News: unsubscribed user {update.message.from_user.username}')
     chat_id = update.message.chat_id
     subscribed_chats.remove(chat_id)
 
