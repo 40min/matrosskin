1c46c5cede1b34170ef90ed2e9fbea7c52bde050
Andrew Sorokin <40mines@gmail.com>
2019-01-13 00:23:46 +0300

    added ability to set interval in news subscribe

--- matrosskin/paws/news.py
+++ matrosskin/paws/news.py
@@ -1,5 +1,8 @@
 import logging
-from datetime import datetime
+from datetime import (
+    datetime,
+    timedelta
+)
 
 from telegram.ext import CommandHandler
 from telegram.ext.dispatcher import run_async
@@ -16,7 +19,8 @@ from .generic import (
 )
 
 DEFAULT_NEWS = 'no news at all (((('
-SUBSCRIBE_PREFIX = 'news_subscribe'
+SUBSCRIBE_PREFIX = 'newssubscribe'
+DEFAULT_NEWS_INTERVAL = 5
 
 logger = logging.getLogger(__name__)
 
@@ -43,16 +47,18 @@ def grab_news_callback(bot, job):
 
 
 def subscribed_generate_callback(bot, job):
-    for subs in redis_storage.scan(match=f'{SUBSCRIBE_PREFIX}*'):
-        pass
-
-    # stopping here
-
-    # for chat_id in subscribed_chats:
-    #     logger.info(f'News: send news to chat {chat_id}')
-    #     fun_txt = fun_generator.make_phrases()
-    #     if fun_txt:
-    #         bot.send_message(chat_id=chat_id, text=fun_txt[0])
+    for key in redis_storage.scan_iter(match=f'{SUBSCRIBE_PREFIX}_*'):
+        key = str(key, 'utf-8')
+        _, chat_id = key.split('_')
+        subscription_data = redis_storage.hgetall(key)
+        interval = int(subscription_data[b'interval'])
+        last_generated = int(subscription_data[b'last_generated'])
+        if datetime.fromtimestamp(last_generated) + timedelta(seconds=interval) < datetime.utcnow():
+            logger.info(f'News: send news to chat {chat_id}')
+            fun_txt = fun_generator.make_phrases()
+            if fun_txt:
+                bot.send_message(chat_id=chat_id, text=fun_txt[0])
+            redis_storage.hset(key, 'last_generated', int(datetime.utcnow().timestamp()))
 
 
 @run_async
@@ -63,11 +69,26 @@ def show_news(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=news_phrase)
 
 
+def get_message_interval(update):
+    interval_txt = update.effective_message['text'].replace('/subs', '').strip()
+    if not interval_txt:
+        return DEFAULT_NEWS_INTERVAL
+    try:
+        return int(interval_txt)
+    except ValueError:
+        return DEFAULT_NEWS_INTERVAL
+
+
 def subscribe(bot, update):
     logger.info(f'News: subscribed user {update.message.from_user.username}')
     chat_id = update.message.chat_id
     key = get_storage_subscribe_key(chat_id)
-    redis_storage.hmset(key, {'chat_id': chat_id})
+    news_interval = get_message_interval(update)
+    redis_storage.hmset(key, {
+            'interval': news_interval,
+            'last_generated': int(datetime.utcnow().timestamp())
+        }
+    )
 
 
 def unsubscribe(bot, update):
@@ -86,5 +107,5 @@ class NewsPaw(Paw):
     }
     jobs = {
         Job(callback=grab_news_callback, interval=60*60*6, first=0),
-        Job(callback=subscribed_generate_callback, interval=5, first=1)
+        Job(callback=subscribed_generate_callback, interval=5, first=0)
     }
